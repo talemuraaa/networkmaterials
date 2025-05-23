61c6204db7581c6a4ed1fdfbe0d166f74b708bdf
takemura <csna25008@g.nihon-u.ac.jp>
2025-05-23 15:35:30 +0900

    Add files via upload

---
+++ utils/sideber_title.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+#サイドバーのページタイトルを日本語で表示する
+
+def sideber_title(): 
+    with st.sidebar:
+        st.page_link("app.py",label="HOME",icon="🏠")
+        st.page_link("pages/programs.py",label="ネットワーク保管庫",icon="🔨")
+        st.page_link("pages/MATERIALS.py",label="発表資料",icon="📖")        
+        st.page_link("pages/RECORD.py",label="アーカイブ",icon="📚")
+    
+    st.sidebar.divider()
